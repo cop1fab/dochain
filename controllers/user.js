35e48943ff166e8c02e72a5a8d366a8ed99a063e
cop1fab <bienaimecopain@gmail.com>
2019-05-11 11:36:35 +0200

    user login

--- controllers/user.js
+++ controllers/user.js
@@ -1,24 +1,32 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import { user } from '../database/models';
+import {
+  user
+} from '../database/models';
 
 dotenv.config();
 
-class User {
+export default class User {
   static async create(req, res) {
     try {
       req.body.password = await bcrypt.hash(req.body.password, 10);
 
       const result = await user.create(req.body);
 
-      const { email, type } = result;
+      const {
+        email,
+        type
+      } = result;
 
       const token = jwt.sign(req.body, process.env.SECRET);
 
       return res.status(201).json({
         status: 201,
-        user: { email, type },
+        user: {
+          email,
+          type
+        },
         token,
       });
     } catch (e) {
@@ -35,6 +43,33 @@ class User {
       });
     }
   }
-}
 
-export default User;
+  // eslint-disable-next-line class-methods-use-this
+  static async login(req, res) {
+    // eslint-disable-next-line no-shadow
+    const findUser = await user.findOne({
+      where: {
+        email: req.body.email
+      }
+    });
+
+    if (Object.keys(findUser.dataValues).length > 0
+      && bcrypt.compareSync(req.body.password, findUser.dataValues.password)) {
+      const payload = {
+        id: findUser.dataValues.id,
+        email: findUser.dataValues.email
+      };
+      return res.status(200).send({
+        user: findUser.dataValues,
+        token: jwt.sign(payload, process.env.SECRET, {
+          expiresIn: '1d'
+        }),
+        message: 'Welcome back!'
+      });
+    }
+    res.status(400).json({
+      status: 400,
+      error: 'The action wasn/t successful'
+    });
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import user from './user';
 import blockchains from './blockchains';
-import validators from '../middlewares/validators/user';
 
 const router = express.Router();
 
 router.use('/blockchains', blockchains);
-router.use('/auth', validators.create, user.create);
+router.use('/auth', user);
 
 export default router;
